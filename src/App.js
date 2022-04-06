0108b46eae95ad0b68abbe9bb358d848bb574ba0
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-04-06 15:51:18 -0600

    React routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+
+
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' element={<div>home page</div>} />
-      <Route
-        path='testing'
-        element={<div>
-          <h2>testing</h2>
-        </div>}
-      >
-      </Route>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
     </BrowserRouter >
 
   )
