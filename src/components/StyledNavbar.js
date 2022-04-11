0cd908d940daf508597071f078024497349b1963
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-04-11 16:06:12 -0600

    reactjs routing

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <NavLink to="/" style={(isActive) => isActive ? 'link active' : 'link'}
+            >Home</NavLink>
+            <NavLink to="/about" >About</NavLink>
+            <NavLink to="/" >Products</NavLink>
+        </nav>
+    )
+}
+
+export default Navbar;
