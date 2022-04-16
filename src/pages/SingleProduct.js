0712fab744a1d90cb29869055f27963e2afb62f5
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-04-16 15:05:43 -0600

    reactjs routing

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,6 +1,8 @@
 import { Link, useParams } from 'react-router-dom'
+import products from '../data'
 const SingleProduct = () => {
   const { productId } = useParams();
+  const product = products.find((product) => product.id === productId)
   return (
     <section className='section product'>
       <h4>{productId}</h4>
