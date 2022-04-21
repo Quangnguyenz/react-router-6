2b0b8b2550830abddb57f933b04819a6954a9534
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-04-20 19:43:59 -0600

    Reactjs Routing

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path='products' element={<Products />} />
           <Route path='/products/:productId' element={<SingleProduct />}></Route>
           <Route path='login' element={<Login setUser={setUSer}></Login>}></Route>
-          <Route path='dashboard' element={<Dashboard user={user}></Dashboard>}></Route>
+          <Route path='dashboard' element={<ProtectedRoute user={user}></ProtectedRoute>}></Route>
           <Route path="*" element={<Error />}></Route>
         </Route>
       </Routes>
