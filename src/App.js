55564566e511f3c40e0073057ce456b2de54845c
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-04-21 18:20:12 -0600

    ReactJS Routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Products from './pages/Products'
 import Error from './pages/Error'
 import SharedLayout from './pages/SharedLayout'
 import SingleProduct from './pages/SingleProduct'
-
+import SharedProducLayout from './pages/SharedProductLayout'
 function App() {
   const [user, setUser] = useState(null)
   return (
@@ -15,8 +15,9 @@ function App() {
         <Route path='/' element={<SharedLayout />} >
           <Route index element={<Home />}></Route>
           <Route path='about' element={<About />} />
+          <Route path='products' element={<SharedProducLayout />} />
           <Route path='products' element={<Products />} />
-          <Route path='/products/:productId' element={<SingleProduct />}></Route>
+          <Route path=':productId' element={<SingleProduct />}></Route>
           <Route path='login' element={<Login setUser={setUSer}></Login>}></Route>
           <Route path='dashboard' element={<ProtectedRoute user={user}></ProtectedRoute>}></Route>
           <Route path="*" element={<Error />}></Route>
